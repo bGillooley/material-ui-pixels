3d4b8a394ba41e5bd4c08ebfc7029cc31c4870cf
unknown <gillooley@gmail.com>
2021-08-05 21:44:13 +0100

    Editing from laptop

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -7,14 +7,14 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. And something like that.
+          Edit <code>src/App.js</code> and save to reload. And something like
+          that. And something else
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
